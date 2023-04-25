031b95759439736075c1d6a706adea1657a93e35
Nikolay Kandalintsev <nicloay@gmail.com>
2023-04-25 12:48:28 +0200

    Add another todo for EngineApiAdapter

--- Assets/Scripts/JSInterop/EngineApiAdapter.cs
+++ Assets/Scripts/JSInterop/EngineApiAdapter.cs
@@ -5,6 +5,7 @@ namespace JSInterop
     /// <summary>
     ///     ClearScript struggle to add host object through interface.
     ///     Use this adapter to forward the call to actual IEngineApi implementation
+    ///     TODO: if we don't use UniTask we can remove this adapter and use IEngineApi directly
     /// </summary>
     public class EngineApiAdapter
     {
